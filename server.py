e132d997e1709f2c1e2b877e4eb0611ac6edb940
William Mayor <mail@williammayor.co.uk>
2013-04-15 19:53:24 +0100

    Refresh

--- server.py
+++
@@ -1,9 +0,0 @@
-import socket
-import utils
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', 5353))
-s.listen(5)
-while 1:
-    (cs, ca) = s.accept()
-    utils.prettyprint(*utils.receive(cs))
