54fc3811dde7bd4a0ad03a0947ff75905fe63aa7
William Mayor <mail@williammayor.co.uk>
2013-04-17 13:10:58 +0100

    Console output

--- script.py
+++ script.py
@@ -1,16 +1,24 @@
+import html
+
+def info(message):
+    doc["console_output"] <= html.LI(message, CLASS='info')
+
+def success(message):
+    doc["console_output"] <= html.LI(message, CLASS='success')
+
 def error(message):
-    print(message)
-    doc["console_output"] <= LI(message)
+    doc["console_output"] <= html.LI(message, CLASS='error')
 
 def run_encrypt():
-    print('run_encrypt')
     src = doc["encrypt_code"].value
     try:
+        info('Loading encryption script')
         exec(src)
+        success('Loaded')
+        plaintext = doc["plaintext"].value
+        info('Encrypting plaintext: ' + plaintext)
+        ciphertext = encrypt(plaintext)
+        success('Encrypted into: ' + ciphertext)
+        doc["ciphertext"].innerText = ciphertext
     except Exception as exc:
-        error(traceback.print_exc())
-    plaintext = doc["plaintext"].value
-    print(plaintext)
-    ciphertext = encrypt(plaintext)
-    print(ciphertext)
-    doc["ciphertext"].innerText = ciphertext
+        error(exc)
