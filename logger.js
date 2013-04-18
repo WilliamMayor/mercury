5912e78f064c7554efb45d6273b9a87d48dc1917
William Mayor <mail@williammayor.co.uk>
2013-04-18 11:05:57 +0100

    Remove codemirror stuff

--- logger.js
+++
@@ -1,9 +0,0 @@
-error = function(message) {
-    $("#console_output").append($("<li class='error'>" + message + "</li>"));
-}
-success = function(message) {
-    $("#console_output").append($("<li class='success'>" + message + "</li>"));
-}
-info = function(message) {
-    $("#console_output").append($("<li class='info'>" + message + "</li>"));
-}
